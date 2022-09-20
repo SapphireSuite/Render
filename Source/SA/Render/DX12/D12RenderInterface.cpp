ff481a68e75c171c1322d49f6f4af85383d6d810
mrouffet <maximerouffet@gmail.com>
2022-09-20 12:26:51 +0900

    [Add] RenderInterface create/destroy logs

--- Source/SA/Render/DX12/D12RenderInterface.cpp
+++ Source/SA/Render/DX12/D12RenderInterface.cpp
@@ -2,15 +2,20 @@
 
 #include <DX12/D12RenderInterface.hpp>
 
+#include <SA/Collections/Debug>
+
 namespace SA
 {
 	namespace DX12
 	{
 		void RenderInterface::Create()
 		{
+			SA_LOG(L"Render Interface created.", Infos, SA/Render/DX12);
 		}
+
 		void RenderInterface::Destroy()
 		{
+			SA_LOG(L"Render Interface destroyed.", Infos, SA/Render/DX12);
 		}
 	}
 }
--- Source/SA/Render/OpenGL/VkRenderInterface.cpp
+++ Source/SA/Render/OpenGL/VkRenderInterface.cpp
@@ -2,15 +2,20 @@
 
 #include <OpenGL/GLRenderInterface.hpp>
 
+#include <SA/Collections/Debug>
+
 namespace SA
 {
 	namespace GL
 	{
 		void RenderInterface::Create()
 		{
+			SA_LOG(L"Render Interface created.", Infos, SA/Render/OpenGL);
 		}
+
 		void RenderInterface::Destroy()
 		{
+			SA_LOG(L"Render Interface destroyed.", Infos, SA/Render/OpenGL);
 		}	
 	}
 }
