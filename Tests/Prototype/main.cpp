36c55f2935f716adb1ef77f5c57b6a659160c1b8
Maxime ROUFFET <maximerouffet@gmail.com>
2022-09-25 17:50:31 +0900

    [Add] Prototype tests

---
+++ Tests/Prototype/main.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#include <SA/Collections/Debug>
+using namespace SA;
+
+#include <SCommon/WindowRenderApplication.hpp>
+
+class SampleApp : public SCommon::WindowRenderApplication
+{
+protected:
+	void Init() override final
+	{
+		SCommon::WindowRenderApplication::Init();
+	}
+
+	void UnInit() override final
+	{
+		SCommon::WindowRenderApplication::UnInit();
+	}
+
+	void Loop() override final
+	{
+		SCommon::WindowRenderApplication::Loop();
+	}
+
+	void Update() override final
+	{
+		SCommon::WindowRenderApplication::Update();
+	}
+};
+
+int main()
+{
+	SA::Debug::InitDefaultLogger();
+
+	SampleApp app;
+
+	return app.CreateAndRun<SCommon::SelectedWindowInterface, SCommon::SelectedRenderInterface>();
+}
